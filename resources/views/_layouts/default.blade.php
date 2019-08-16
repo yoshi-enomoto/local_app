8bd0cb58ee9de7e95d495f2b27b40743eed171e4
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-08-16 16:00:40 +0900

    サイドバーの切り出し

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -30,26 +30,8 @@
 
         <!-- トップメニュー -->
         @include('_components.header')
-
-        @if(config('adminlte.layout') != 'top-nav')
-        <!-- Left side column. contains the logo and sidebar -->
-        <aside class="main-sidebar">
-
-            {{-- 書き換える為、下記すべてを入れ替え --}}
-            <section class="sidebar">
-                <ul class="sidebar-menu">
-
-                    <!-- メニューヘッダ -->
-                    <li class="header">機能一覧</li>
-
-                    <!-- メニュー項目 -->
-                    <li><a href="">新規登録</a></li>
-
-                </ul>
-            </section>
-
-        </aside>
-        @endif
+        {{-- サイドバー --}}
+        @include('_components.sidebar')
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
