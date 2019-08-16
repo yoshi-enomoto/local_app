173292061bc328934fed3f1ae2fb22f8c8b19cad
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-08-16 17:27:29 +0900

    コンテンツ以外の切り分け

---
+++ resources/views/_components/js.blade.php
@@ -0,0 +1,26 @@
+<script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- adminLTE -->
+{{-- 下記がないとサイドバーが展開しない --}}
+<script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+
+@if(config('adminlte.plugins.select2'))
+    <!-- Select2 -->
+    {{-- 初期の状態から『https:』を付加する --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+@endif
+
+@if(config('adminlte.plugins.datatables'))
+    <!-- DataTables with bootstrap 3 renderer -->
+    {{-- 初期の状態から『https:』を付加する --}}
+    <script src="https://cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
+@endif
+
+@if(config('adminlte.plugins.chartjs'))
+    <!-- ChartJS -->
+    {{-- 初期の状態から『https:』を付加する --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
+@endif
+
+@yield('adminlte_js')
--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -1,50 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>@yield('title_prefix', config('adminlte.title_prefix', ''))
-        @yield('title', config('adminlte.title', 'AdminLTE 2'))
-        @yield('title_postfix', config('adminlte.title_postfix', ''))</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/bootstrap/dist/css/bootstrap.min.css') }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/font-awesome/css/font-awesome.min.css') }}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/vendor/Ionicons/css/ionicons.min.css') }}">
-
-    @if(config('adminlte.plugins.select2'))
-        <!-- Select2 -->
-        {{-- 初期の状態から『https:』を付加する --}}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css">
-    @endif
-
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
-
-    @if(config('adminlte.plugins.datatables'))
-        <!-- DataTables with bootstrap 3 style -->
-        {{-- 初期の状態から『https:』を付加する --}}
-        <link rel="stylesheet" href="https://cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css">
-    @endif
-
-    {{-- 下記の1行を yieldで導入するかしないかで変わる --}}
-    <link rel="stylesheet"
-          href="{{ asset('vendor/adminlte/dist/css/skins/skin-yellow.min.css')}} ">
-
-    @yield('adminlte_css')
-        {{-- 出力では『/css/admin_custom.css』になるので、独自に追加する用？ --}}
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    @include('_components.head')
+    @include('_components.css')
 </head>
+
 {{-- <body class="hold-transition @yield('body_class')"> --}}
 <!-- <body class="skin-yellow"> -->
 <body class="skin-yellow">
@@ -173,39 +133,9 @@
 
         <!-- フッター -->
         <footer class="main-footer">
-            <div class="pull-right hidden-xs">Version1.0</div>
-            <strong>Copyright &copy; 2015</strong>, All rights reserved.
-        </footer><!-- end footer -->
-
-
+            @include('_components.footer')
+        </footer>
     </div><!-- end wrapper -->
-
-    <script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/adminlte/vendor/jquery/dist/jquery.slimscroll.min.js') }}"></script>
-    <script src="{{ asset('vendor/adminlte/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <!-- adminLTE -->
-    {{-- 下記がないとサイドバーが展開しない --}}
-    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
-
-    @if(config('adminlte.plugins.select2'))
-        <!-- Select2 -->
-        {{-- 初期の状態から『https:』を付加する --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-    @endif
-
-    @if(config('adminlte.plugins.datatables'))
-        <!-- DataTables with bootstrap 3 renderer -->
-        {{-- 初期の状態から『https:』を付加する --}}
-        <script src="https://cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
-    @endif
-
-    @if(config('adminlte.plugins.chartjs'))
-        <!-- ChartJS -->
-        {{-- 初期の状態から『https:』を付加する --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js"></script>
-    @endif
-
-    @yield('adminlte_js')
-
+    @include('_components.js')
 </body>
 </html>
