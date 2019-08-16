14d2dfc394e3f7c3c22997a39a6000cba62858d1
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-08-16 16:51:12 +0900

    やり直しのためにdelete

--- resources/views/_components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- フッター -->
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">Version1.0</div>
-    <strong>Copyright &copy; 2015</strong>, All rights reserved.
-</footer><!-- end footer -->
