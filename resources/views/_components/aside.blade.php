396302f268e0e4d0ac52cb80ae55b75eb9e82c0f
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-08-19 00:16:52 +0900

    サイドバーアイコンの修正

--- resources/views/_components/aside.blade.php
+++ resources/views/_components/aside.blade.php
@@ -4,25 +4,25 @@
         <li class="header">機能一覧</li>
         <!-- メニュー項目 -->
         <li class="treeview">
-            <a href="#"><i class="fa fa-circle-o"></i><span>カテゴリー管理</span><i class="fa fa-angle-left pull-right"></i></a>
+            <a href="#"><i class="fa fa-star"></i><span>カテゴリー管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('mock_categories', ['index']) }}">カテゴリー一覧</a></li>
-                <li><a href="{{ route('mock_categories', ['create']) }}">カテゴリー登録</a></li>
+                <li><a href="{{ route('mock_categories', ['index']) }}"><i class="fa fa-circle-o"></i>カテゴリー一覧</a></li>
+                <li><a href="{{ route('mock_categories', ['create']) }}"><i class="fa fa-circle-o"></i>カテゴリー登録</a></li>
             </ul>
         </li>
         <li class="treeview">
-            <a href="#"><i class="fa fa-circle-o"></i><span>タスク管理</span><i class="fa fa-angle-left pull-right"></i></a>
+            <a href="#"><i class="fa fa-star"></i><span>タスク管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('mock_tasks', ['index']) }}">タスク一覧</a></li>
-                <li><a href="{{ route('mock_tasks', ['create']) }}">タスク登録</a></li>
+                <li><a href="{{ route('mock_tasks', ['index']) }}"><i class="fa fa-circle-o"></i>タスク一覧</a></li>
+                <li><a href="{{ route('mock_tasks', ['create']) }}"><i class="fa fa-circle-o"></i>タスク登録</a></li>
             </ul>
         </li>
         <li class="treeview">
-            <a href="#"><i class="fa fa-circle-o"></i><span>時間数管理</span><i class="fa fa-angle-left pull-right"></i></a>
+            <a href="#"><i class="fa fa-star"></i><span>時間数管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('mock_hours', ['create']) }}">時間入力</a></li>
-                <li><a href="{{ route('mock_hours', ['index_this_month']) }}">当月一覧</a></li>
-                <li><a href="{{ route('mock_hours', ['index_month']) }}">月一覧</a></li>
+                <li><a href="{{ route('mock_hours', ['create']) }}"><i class="fa fa-circle-o"></i>時間入力</a></li>
+                <li><a href="{{ route('mock_hours', ['index_this_month']) }}"><i class="fa fa-circle-o"></i>当月一覧</a></li>
+                <li><a href="{{ route('mock_hours', ['index_month']) }}"><i class="fa fa-circle-o"></i>月一覧</a></li>
             </ul>
         </li>
     </ul>
