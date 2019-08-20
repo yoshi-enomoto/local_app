366ce52289cefb54d1391e3e76d8eff7c21f829e
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-08-21 06:45:10 +0900

    Migrationファイルたち

---
+++ database/migrations/2019_08_20_203450_create_hours_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->float('hour', 5, 3 );
+                // 要確認！
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('task_id')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table
+                ->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+            $table
+                ->foreign('task_id')
+                ->references('id')
+                ->on('tasks')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours');
+    }
+}
