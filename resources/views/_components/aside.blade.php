34c0b1dd488e34aebe7aa9cf227ea812b4528bb9
榎本 芳昭 <yoshi.another.ac@gmail.com>
2019-09-20 00:41:53 +0900

    サイドバーを開いている時にアクティブにする

--- resources/views/_components/aside.blade.php
+++ resources/views/_components/aside.blade.php
@@ -3,26 +3,26 @@
         <!-- メニューヘッダ -->
         <li class="header">機能一覧</li>
         <!-- メニュー項目 -->
-        <li class="treeview">
+        <li class="treeview {{ request()->is('categories', 'categories/*') ? 'active' : '' }}">
             <a href="#"><i class="fa fa-star"></i><span>カテゴリー管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('categories.index') }}"><i class="fa fa-circle-o"></i>カテゴリー一覧</a></li>
-                <li><a href="{{ route('categories.create') }}"><i class="fa fa-circle-o"></i>カテゴリー登録</a></li>
+                <li class="{{ request()->is('categories') ? 'active' : '' }}"><a href="{{ route('categories.index') }}"><i class="fa fa-circle-o"></i>カテゴリー一覧</a></li>
+                <li class="{{ request()->is('categories/create') ? 'active' : '' }}"><a href="{{ route('categories.create') }}"><i class="fa fa-circle-o"></i>カテゴリー登録</a></li>
             </ul>
         </li>
-        <li class="treeview">
+        <li class="treeview {{ request()->is('tasks', 'tasks/*') ? 'active' : '' }}">
             <a href="#"><i class="fa fa-star"></i><span>タスク管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('tasks.index') }}"><i class="fa fa-circle-o"></i>タスク一覧</a></li>
-                <li><a href="{{ route('tasks.create') }}"><i class="fa fa-circle-o"></i>タスク登録</a></li>
+                <li class="{{ request()->is('tasks') ? 'active' : '' }}"><a href="{{ route('tasks.index') }}"><i class="fa fa-circle-o"></i>タスク一覧</a></li>
+                <li class="{{ request()->is('tasks/create') ? 'active' : '' }}"><a href="{{ route('tasks.create') }}"><i class="fa fa-circle-o"></i>タスク登録</a></li>
             </ul>
         </li>
-        <li class="treeview">
+        <li class="treeview {{ request()->is('hours', 'hours/*') ? 'active' : '' }}">
             <a href="#"><i class="fa fa-star"></i><span>時間数管理</span><i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ route('hours.create') }}"><i class="fa fa-circle-o"></i>時間入力</a></li>
-                <li><a href="{{ route('hours.list_date') }}"><i class="fa fa-circle-o"></i>当月一覧</a></li>
-                <li><a href="{{ route('hours.list_month') }}"><i class="fa fa-circle-o"></i>月一覧</a></li>
+                <li class="{{ request()->is('hours/create') ? 'active' : '' }}"><a href="{{ route('hours.create') }}"><i class="fa fa-circle-o"></i>時間入力</a></li>
+                <li class="{{ request()->is('hours/list_date') ? 'active' : '' }}"><a href="{{ route('hours.list_date') }}"><i class="fa fa-circle-o"></i>当月一覧</a></li>
+                <li class="{{ request()->is('hours/list_month') ? 'active' : '' }}"><a href="{{ route('hours.list_month') }}"><i class="fa fa-circle-o"></i>月一覧</a></li>
 
                 @if(Config::get('app.mode') == 'dev')
                     <li><a href="{{ route('mock_hours', ['create']) }}"><i class="fa fa-circle-o"></i>時間入力</a></li>
